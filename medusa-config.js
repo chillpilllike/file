927f19de7305799d9d3d4f4f3fd0711b3a60a10d
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-07-17 23:01:08 +0530

    Update medusa-config.js

--- medusa-config.js
+++ medusa-config.js
@@ -61,13 +61,6 @@ const plugins = [
       enableUI: true,
     },
   },
-  {
-    resolve: `medusa-payment-stripe`,
-    options: {
-      api_key: process.env.STRIPE_API_KEY,
-      webhook_secret: process.env.STRIPE_WEBHOOK_SECRET,
-    },
-  },
 ];
 
 const modules = {
