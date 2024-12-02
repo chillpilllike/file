9a26a33beae6fe3e294baf3ea92dca59ca7ed667
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-12-02 14:59:54 +0530

    Update barrio.js

--- barrio.js
+++ barrio.js
@@ -11,8 +11,7 @@ module.exports = defineConfig({
     databaseDriverOptions: {
       ssl: false,
     },
-    // redisUrl: REDIS_URL,
-    redisUrl: process.env.REDIS_URL, // Added redisUrl
+    redisUrl: REDIS_URL,
     redisPrefix: process.env.REDIS_PREFIX,
     http: {
       storeCors: process.env.STORE_CORS,
@@ -55,6 +54,25 @@ module.exports = defineConfig({
         redis: {
           url: REDIS_URL,
         },
+      },
+    },
+    {
+      resolve: "@medusajs/medusa/file",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/file-s3",
+            id: "s3",
+            options: {
+              file_url: process.env.S3_FILE_URL,
+              access_key_id: process.env.S3_ACCESS_KEY_ID,
+              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
+              region: process.env.S3_REGION,
+              bucket: process.env.S3_BUCKET,
+              endpoint: process.env.S3_ENDPOINT,
+            },
+          },
+        ],
       },
     },
   ],
